2770643383131b80f9c08354c4977f8fda25cb29
Ekremunalytu <102416114+Ekremunalytu@users.noreply.github.com>
2025-12-11 10:24:00 +0300

    get extension info api's added

--- routers/core.py
+++ routers/core.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from typing import List
 
-from schemas.schemas import ExtensionSchema, ScanRequest, SearchRequest
+from schemas.schemas import ExtensionSchema, ScanRequest, SearchRequest, SearchAllExtensionsInfo, SearchAllExtensionsAllInfo
 from core.deps import get_db
 from scanner import service
 
@@ -38,6 +38,34 @@ def search_extension(
     except Exception as e:
         print(f"Error in search_extension: {e}")
         raise HTTPException(status_code=500, detail=f"Internal Server Error: {str(e)}")
+
+@router.get("/getExtensionsBaseInfo", response_model=List[SearchAllExtensionsInfo])
+def get_extensions_base_info(db: Session = Depends(get_db)):
+    try:
+        result = service.get_all_extensions_basic(db)
+        if result is None:
+            raise HTTPException(status_code=404, detail="Extension not found")
+        return result
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    except Exception as e:
+        print(f"Error in get_extensions_base_info: {e}")
+        raise HTTPException(status_code=500, detail=f"Internal Server Error: {str(e)}")
+
+@router.get("/getExtensionsAllInfo", response_model=List[SearchAllExtensionsAllInfo])
+def get_extensions_all_info(db: Session = Depends(get_db)):
+    try:
+        result = service.get_all_extensions_all(db)
+        if result is None:
+            raise HTTPException(status_code=404, detail="Extension not found")
+        return result
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    except Exception as e:
+        print(f"Error in get_extensions_all_info: {e}")
+        raise HTTPException(status_code=500, detail=f"Internal Server Error: {str(e)}")
+
+
 
 @router.post("/createExtension", response_model=ExtensionSchema)
 def create_extension(request: ScanRequest, db: Session = Depends(get_db)):
